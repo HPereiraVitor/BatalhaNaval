5125e195f19ef19eb276d42f98c4d0d6d8375ed8
znogvic <vnogueira@fitec.org.br>
2023-05-23 17:36:36 -0300

    a

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import re #regex
+p1_boats = []
+p2_boats = []
+ref = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P"]
 
 # 15x15 Tabuleiro
 class Board:
@@ -51,20 +54,34 @@ class Player:
     def __init__(self, file: str) -> None:
         self.file = open(file, 'r')
         self.board = Board()
+        self.peca_pos = {}
+        self.readfile()
 
     def readfile(self):
-        for line in self.file.readline():
+        for line in self.file:
             # Usando REGEX
             # 1;A2V|C7H
-            self.id_peca    = re.search('.(?=\;)', line)
-            self.pos_peca   = line.split(";").pop(0)[0].split('|')
+            id_peca    = re.search('.(?=\;)', line).group()
+            self.peca_pos.update({id_peca: {}})
+            pos_peca   = line.split(";")[1].replace('\n', '').split('|')
+            for i, n in enumerate(pos_peca):
+                self.peca_pos[id_peca].update({i: {'places': [n[:-1]], 'orientation': [n[-1]]}})
+
+
+            print(self.peca_pos)
+
 
 class Game:
     def __init__(self, j1, j2) -> None:
         self.j1 = j1
         self.j2 = j2
+        self.winner()
+
+    def winner(self):
+        with open('resultado.txt', 'w') as f:
+            f.write("")
 
 if __name__ == '__main__':
-    p1 = Player()
-    p2 = Player()
+    p1 = Player('p1.txt')
+    p2 = Player('p2.txt')
     game = Game(p1, p2)
