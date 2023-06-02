33e3fc326402293f28ae8db9720a10873852f18e
HPereiraVitor <vitor.pereira11@fatec.sp.gov.br>
2023-06-02 14:19:52 -0300

    Append Players

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ class Error_Handle:
         pass
 
     def check_piece_qtty(self, qtty, max_qtty) -> None:
-        if qtty >= max_qtty:
+        if qtty > max_qtty:
             raise Exception("ERROR_NR_PARTS_VALIDATION")
-    
+
     def check_piece_ovw(self, pos_a, pos_b) -> None:
         if pos_a == pos_b:
             raise Exception("ERROR_OVERWRITE_PIECES_VALIDATION")
-        
+
     def check_piece_pos(self, pos) -> None:
         if (pos[0] not in ref) or (int(pos[1:-1]) >= 15):
             raise Exception("ERROR_POSITION_NONEXISTENT_VALIDATION")
@@ -71,9 +71,10 @@ class Board:
         }
 
 class Player:
-    def __init__(self, file: str) -> None:
+    def __init__(self, file: str, playerId: int) -> None:
         self.file = open(file, 'r')
         self.board = Board()
+        self.pId = playerId
         self.peca_pos = {}
         self.error_handle = Error_Handle()
         self.readfile()
@@ -82,53 +83,68 @@ class Player:
         for line in self.file:
             if line[0] in ("1","2","3","4") and not line.startswith("#"):
                 # Usando REGEX
-                # 1;A2V|C7H
                 id_peca    = re.search('.(?=\;)', line).group()
                 self.peca_pos.update({id_peca: {}})
                 pos_peca   = line.split(";")[1].replace('\n', '').split('|')
                 self.error_handle.check_piece_qtty(len(pos_peca), self.board.boats[id_peca]['qtd'])
-                
+
                 for i, n in enumerate(pos_peca):
                     self.peca_pos[id_peca].update({i: []})
                     idx = 0
                     start_pos_l = n[0]
                     start_pos_l_idx = ref.index(start_pos_l)
                     start_pos_n = int(n[1:-1]) if n.endswith("V") or n.endswith("H") else int(n[1:])
-                
+
                     # self.error_handle.check_piece_pos(n)
                     if id_peca == "1":
                         while idx < self.board.boats[id_peca]['pos']:
                             count = idx+1
-                
+
                             if n[-1] == 'V':
                                 if start_pos_l_idx+idx >= len(ref):
                                     raise Exception("ERROR_POSITION_NONEXISTENT_VALIDATION")
                                 else:
                                     self.peca_pos[id_peca][i].append(f'{ref[start_pos_l_idx+idx]}{start_pos_n}')
-                
+                                    if self.pId == 1:
+                                        p1_boats.append(f'{ref[start_pos_l_idx+idx]}{start_pos_n}')
+                                    else:
+                                        p2_boats.append(f'{ref[start_pos_l_idx+idx]}{start_pos_n}')
+
                             elif n[-1] == 'H':
                                 if start_pos_n+(idx) >= len(ref):
                                     raise Exception("ERROR_POSITION_NONEXISTENT_VALIDATION")
                                 else:
                                     self.peca_pos[id_peca][i].append(f'{start_pos_l}{start_pos_n+(idx)}')
+                                    if self.pId == 1:
+                                        p1_boats.append(f'{ref[start_pos_l_idx+idx]}{start_pos_n}')
+                                    else:
+                                        p2_boats.append(f'{ref[start_pos_l_idx+idx]}{start_pos_n}')
 
                             idx += 1
 
                     elif id_peca == "2":
                         while idx < self.board.boats[id_peca]['pos']:
-                            count = idx+1
+                            count = idx
 
                             if n[-1] == 'V':
                                 if start_pos_l_idx+count >= len(ref):
                                     raise Exception("ERROR_POSITION_NONEXISTENT_VALIDATION")
                                 else:
                                     self.peca_pos[id_peca][i].append(f'{ref[start_pos_l_idx+idx]}{start_pos_n}')
-                
+                                    if self.pId == 1:
+                                        p1_boats.append(f'{ref[start_pos_l_idx+idx]}{start_pos_n}')
+                                    else:
+                                        p2_boats.append(f'{ref[start_pos_l_idx+idx]}{start_pos_n}')
+
                             elif n[-1] == 'H':
                                 if start_pos_n+(idx+1) >= len(ref):
                                     raise Exception("ERROR_POSITION_NONEXISTENT_VALIDATION")
                                 else:
                                     self.peca_pos[id_peca][i].append(f'{start_pos_l}{start_pos_n+(idx)}')
+                                    if self.pId == 1:
+                                        p1_boats.append(f'{ref[start_pos_l_idx+idx]}{start_pos_n}')
+                                    else:
+                                        p2_boats.append(f'{ref[start_pos_l_idx+idx]}{start_pos_n}')
 
                             idx += 1
 
@@ -140,6 +156,10 @@ class Player:
                                 raise Exception("ERROR_POSITION_NONEXISTENT_VALIDATION")
                             else:
                                 self.peca_pos[id_peca][i].append(n)
+                                if self.pId == 1:
+                                    p1_boats.append(n)
+                                else:
+                                    p2_boats.append(n)
 
                             idx += 1
 
@@ -152,18 +172,27 @@ class Player:
                                     raise Exception("ERROR_POSITION_NONEXISTENT_VALIDATION")
                                 else:
                                     self.peca_pos[id_peca][i].append(f'{ref[start_pos_l_idx+idx]}{start_pos_n}')
-                
+                                    if self.pId == 1:
+                                        p1_boats.append(f'{ref[start_pos_l_idx+idx]}{start_pos_n}')
+                                    else:
+                                        p2_boats.append(f'{ref[start_pos_l_idx+idx]}{start_pos_n}')
+
                             elif n[-1] == 'H':
                                 if start_pos_n+(idx+1) >= len(ref):
                                     raise Exception("ERROR_POSITION_NONEXISTENT_VALIDATION")
                                 else:
                                     self.peca_pos[id_peca][i].append(f'{start_pos_l}{start_pos_n+(idx)}')
+                                    if self.pId == 1:
+                                        p1_boats.append(f'{start_pos_l}{start_pos_n+(idx)}')
+                                    else:
+                                        p2_boats.append(f'{start_pos_l}{start_pos_n+(idx)}')
 
                             idx += 1
 
 
 
-            print(self.peca_pos)
+            print(p1_boats)
+            print(p2_boats)
 
 
 class Game:
@@ -177,6 +206,6 @@ class Game:
             f.write("")
 
 if __name__ == '__main__':
-    p1 = Player('p1.txt')
-    p2 = Player('p2.txt')
+    p1 = Player('p1.txt', 1)
+    p2 = Player('p2.txt', 2)
     game = Game(p1, p2)
